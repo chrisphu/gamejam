c3548207d5985f644ef5fc3453048bdf8de1ca56
briancatter <catter.brian@gmail.com>
2022-06-24 22:14:58 -0500

    WIP collision and joint script

--- Assets/Scripts/CustomBoxCollider.cs
+++ Assets/Scripts/CustomBoxCollider.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CustomBoxCollider : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Vector3 positionDifference;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,8 +19,28 @@ public class CustomBoxCollider : MonoBehaviour
         
     }
 
+    void Awake()
+    {
+#if UNITY_EDITOR
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 45;
+#endif
+    }
+
     void FixedUpdate()
     {
+        if (rb.GetComponent<FixedJoint2D>().connectedBody is Rigidbody2D player)
+        {
+            rb.transform.position = player.transform.position + positionDifference;
+        }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.rigidbody.gameObject.CompareTag("Player"))
+        {
+            collision.otherRigidbody.GetComponent<FixedJoint2D>().connectedBody = collision.rigidbody;
+            positionDifference = collision.rigidbody.position = collision.otherRigidbody.position;
+        }
     }
 }
